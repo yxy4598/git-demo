955d647e8d707a09899119cf6df6b854fe76da92
coderyxy <yu.life@qq.com>
2022-09-25 23:39:56 +0800

    package tabBar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="app"></div>
+  <div class="app">
+    <router-view></router-view>
+    <tab-bar></tab-bar>
+  </div>
 </template>
 
 <script setup>
-  
+  import tabBar from './components/tab-bar/tab-bar.vue';
+
 </script>
 
 <style scoped>
